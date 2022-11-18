637f4281ff2f61f314c3a3abf99956d684c95a38
Vyacheslav Zhivetyev <slavyan6363@gmail.com>
2022-11-19 02:07:07 +0600

    specified min target for all platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftExtensions",
+	platforms: [
+		.driverKit(.v19),
+		.iOS(.v13),
+		.macCatalyst(.v13),
+		.macOS(.v10_15),
+		.tvOS(.v13),
+		.watchOS(.v6),
+	],
     products: [
         .library(
             name: "FoundationExtensions",
